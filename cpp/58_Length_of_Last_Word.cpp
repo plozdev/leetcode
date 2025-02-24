152ed9e56fbabb95b5027ff621b75c73500b0918
plozdev <azure17171@gmail.com>
2025-02-24 11:20:01 +0700

    ok

---
+++ cpp/58_Length_of_Last_Word.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+class Solution {
+    public:
+        int lengthOfLastWord(string s) {
+            vector <string> v;
+            stringstream ss(s);
+            string word;
+            int count = 0;
+            while (ss>>word) {
+                v.push_back(word);
+                count++;
+            }
+            return v[count-1].size();
+        }
+};
+int main() {    
+    Solution s;
+    cout<<s.lengthOfLastWord(" Hello  World   ");
+    return 0;
+}
