19028af82c3f84fd0dd657d58bcfb9d490eda0a2
plozdev <azure17171@gmail.com>
2025-02-25 00:42:38 +0700

    ok

---
+++ c/58_Length_of_Last_Word.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+int lengthOfLastWord(char* s) {
+    int size = strlen(s);
+    int count = 0;
+    int i = size-1;
+    int flagNum = 0;
+    for (;i>=0;i--) {
+        if (s[i]==' ') {
+            if (!flagNum) continue;
+            else return count;
+        }
+        else {
+            flagNum = 1;
+            count++;
+        }
+    }
+    return count;
+}
+int main() {
+    assert(lengthOfLastWord("Hello World") == 5);
+    assert(lengthOfLastWord("   fly me   to   the moon  ") == 4);
+    assert(lengthOfLastWord("luffy is still joyboy") == 6);
+    assert(lengthOfLastWord("ui a") == 1);
+    assert(lengthOfLastWord(" ") == 0);
+
+    printf("AC");
+    return 0;
+}
---
+++ cs/66_Plus_One.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        digits[digits.Length-1]++;
+        for (int i = digits.Length-1;i>0;i--) {
+            if (digits[i] == 10) {
+                digits[i] = 0;
+                digits[i-1]++;
+            }
+            else break;
+        }
+        if(digits[0]==10) {
+            int[] digit = new int[digits.Length+1];
+            for (int i=digit.Length-1;i>0;i--) {
+                digit[i] = digits[i-1];
+            }
+            digit[1] = 0;
+            digit[0] = 1;
+            return digit;
+        }
+        return digits;
+    }
+}
+class Program {
+    static void Main (string[] args) {
+        int [] a = new int [4] {9,9,9,9};
+        a = Solution.PlusOne(a);
+        foreach (int i in a) Console.Write($"{i} ");
+    }
+}
